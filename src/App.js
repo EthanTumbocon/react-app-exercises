c17495e8ab254ee47a4ea371394863199b311822
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2021-04-14 12:59:13 -0500

    using ES6 with React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a react App</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+      return(
+      <div className="App">
+          <h1>Hi, I'm a react App</h1>
+          <p>This is really working!</p>
+          <Person />
+      </div>
+      );
+      // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Does this work now?'))
+  }
 }
 
 export default App;
